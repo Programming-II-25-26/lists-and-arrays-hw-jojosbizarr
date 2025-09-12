dc4819a671ae57b764aa50854f63f1c6e4e255b0
Abigail Santiago <abigail.santiago@northwestschool.org>
2025-09-12 08:13:13 -0700

    Create push_zeroes.py

---
+++ push_zeroes.py
@@ -0,0 +1,8 @@
+def pushZerosToEnd(arr):
+   #Code goes here
+if __name__ == "__main__":
+    arr = [1, 2, 0, 4, 3, 0, 5, 0]
+    pushZerosToEnd(arr)
+
+    for num in arr:
+        print(num, end=" ")
