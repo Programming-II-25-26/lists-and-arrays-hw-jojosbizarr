81843afda895a18ab5f99c36428d9a4dd1e32d47
Abigail Santiago <abigail.santiago@northwestschool.org>
2025-09-12 08:10:45 -0700

    Create get_alternates.py

---
+++ get_alternates.py
@@ -0,0 +1,10 @@
+#Write a function that prints every alternate element of a given array starting from the first element
+
+def get_alternates(arr):
+    #Enter code here
+
+
+if __name__ == "__main__":
+    arr = [10, 20, 30, 40, 50]
+    res = getAlternates(arr)
+    print(" ".join(map(str, res)))
