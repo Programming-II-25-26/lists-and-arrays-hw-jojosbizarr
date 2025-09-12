27871818d8ab0994f2c36f6c99655b9ac9cb1a9a
Abigail Santiago <abigail.santiago@northwestschool.org>
2025-09-12 08:11:39 -0700

    Create second_largest.py

---
+++ second_largest.py
@@ -0,0 +1,8 @@
+# Write a program to find second largest element in an array
+
+def getSecondLargest(arr):
+   #Code goes here
+
+if __name__ == "__main__":
+    arr = [12, 35, 1, 10, 34, 1]
+    print(getSecondLargest(arr))
